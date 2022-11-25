8fa9eb8105c56832862c979ca934023bd58553c1
fwang356 <fwang356@gatech.edu>
2022-11-25 17:12:13 -0600

    annoying address bar bug go back to old fix even tho it messes up scrolling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { Box, Button, Flex, IconButton, Text, VStack, useColorMode, useMediaQuery, Stack } from "@chakra-ui/react";
-import { FaArrowDown, FaHome, FaSun, FaMoon, FaGithub, FaLinkedin, FaArrowUp } from "react-icons/fa";
+import { FaArrowDown, FaSun, FaMoon, FaGithub, FaLinkedin, FaArrowUp } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import Experience from '../components/Experience';
 import Projects from '../components/Projects';
 import About from '../components/About';
+import Div100vh from 'react-div-100vh';
 
 export default function Home() {
 
@@ -43,44 +44,46 @@ export default function Home() {
 
     return (
         <div>
-            <VStack p={4} h="100vh" w="100%">
-                <Flex w="100%" h="0vh" position="fixed" justifyContent="end">
-                    <IconButton mr={4} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={toggleColorMode} />
-                </Flex>
-                <Flex direction="row"
-                    alignSelf="center" h="full">
-                    <Box align="flex-start" alignSelf="center">
-                        <Text fontSize={isNotSmallerScreen ? "5xl" : "3xl"} fontWeight="semiBold">
-                            Hi, I'm
-                        </Text>
-                        <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, purple.200, purple.400, purple.600)" bgClip="text">
-                            Felix Wang
-                        </Text>
-                        <Text fontSize={isNotSmallerScreen ? "lg" : "sm"} color={isDark ? "gray.200" : "gray.500"}>
-                            Software Engineer | Computer Science Student @ the Georgia Institute of Technology
-                        </Text>
-                        <Button mt={4} color="purple.400" onClick={() =>
-                            window.open("/Resume.pdf")
-                        }>Resume</Button>
-                        <Button mt={4} ml={4} color="purple.400" onClick={() =>
-                            window.open("mailto:fwang356@gatech.edu")
-                        }>Contact Me!</Button>
-                        <br></br>
-                        <IconButton mt={4} icon={<FaGithub />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("https://www.github.com/fwang356")
-                        } />
-                        <IconButton ml={4} mt={4} icon={<FaLinkedin />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("https://www.linkedin.com/in/fwang356")
-                        } />
-                        <IconButton ml={4} mt={4} icon={<HiOutlineMail />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("mailto:fwang356@gatech.edu")
-                        } />
-                    </Box>
-                </Flex>
-                <Stack>
-                    <IconButton mb={16} icon={<FaArrowDown />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollDown} />
-                </Stack>
-            </VStack>
+            <Div100vh>
+                <VStack p={4} h="full" w="100%">
+                    <Flex w="100%" h="0vh" position="fixed" justifyContent="end">
+                        <IconButton mr={4} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={toggleColorMode} />
+                    </Flex>
+                    <Flex p={8} direction="row"
+                        alignSelf="center" h="full">
+                        <Box align="flex-start" alignSelf="center">
+                            <Text fontSize={isNotSmallerScreen ? "5xl" : "3xl"} fontWeight="semiBold">
+                                Hi, I'm
+                            </Text>
+                            <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, purple.200, purple.400, purple.600)" bgClip="text">
+                                Felix Wang
+                            </Text>
+                            <Text fontSize={isNotSmallerScreen ? "lg" : "sm"} color={isDark ? "gray.200" : "gray.500"}>
+                                Software Engineer | Computer Science Student @ the Georgia Institute of Technology
+                            </Text>
+                            <Button mt={4} color="purple.400" onClick={() =>
+                                window.open("/Resume.pdf")
+                            }>Resume</Button>
+                            <Button mt={4} ml={4} color="purple.400" onClick={() =>
+                                window.open("mailto:fwang356@gatech.edu")
+                            }>Contact Me!</Button>
+                            <br></br>
+                            <IconButton mt={4} icon={<FaGithub />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("https://www.github.com/fwang356")
+                            } />
+                            <IconButton ml={4} mt={4} icon={<FaLinkedin />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("https://www.linkedin.com/in/fwang356")
+                            } />
+                            <IconButton ml={4} mt={4} icon={<HiOutlineMail />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("mailto:fwang356@gatech.edu")
+                            } />
+                        </Box>
+                    </Flex>
+                    <Stack>
+                        <IconButton mb={4} icon={<FaArrowDown />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollDown} />
+                    </Stack>
+                </VStack>
+            </Div100vh>
             <Stack alignItems="flex-end">
                 <Flex h="0vh" p={4} position="fixed" bottom="5vh">
                     {isVisible && <IconButton icon={<FaArrowUp />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollToTop} />}
