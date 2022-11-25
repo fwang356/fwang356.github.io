07e6fc859c22a15e10059c581410b2f0234baa47
fwang356 <fwang356@gatech.edu>
2022-11-25 16:35:50 -0600

    mobile card format

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -43,48 +43,50 @@ export default function Home() {
     };
 
     return (
-        <Div100vh>
-            <VStack p={4} h="full" w="100%">
-                <Flex w="100%" h="0vh" position="fixed" justifyContent="end">
-                    <IconButton mr={4} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={toggleColorMode} />
-                </Flex>
+        <div>
+            <Div100vh>
+                <VStack p={4} h="full" w="100%">
 
-                <Flex p={8} direction="row"
-                    alignSelf="center" h="full">
-                    <Box align="flex-start" alignSelf="center">
-                        <Text fontSize={isNotSmallerScreen ? "5xl" : "3xl"} fontWeight="semiBold">
-                            Hi, I'm
-                        </Text>
-                        <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, purple.200, purple.400, purple.600)" bgClip="text">
-                            Felix Wang
-                        </Text>
-                        <Text fontSize={isNotSmallerScreen ? "lg" : "sm"} color={isDark ? "gray.200" : "gray.500"}>
-                            Software Engineer | Computer Science Student @ the Georgia Institute of Technology
-                        </Text>
-                        <Button mt={4} color="purple.400" onClick={() =>
-                            window.open("/Resume.pdf")
-                        }>Resume</Button>
-                        <Button mt={4} ml={4} color="purple.400" onClick={() =>
-                            window.open("mailto:fwang356@gatech.edu")
-                        }>Contact Me!</Button>
-                        <br></br>
-                        <IconButton mt={4} icon={<FaGithub />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("https://www.github.com/fwang356")
-                        } />
-                        <IconButton ml={4} mt={4} icon={<FaLinkedin />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("https://www.linkedin.com/in/fwang356")
-                        } />
-                        <IconButton ml={4} mt={4} icon={<HiOutlineMail />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
-                            window.open("mailto:fwang356@gatech.edu")
-                        } />
-                    </Box>
-                </Flex>
-                <Stack>
-                    <IconButton mb={4} icon={<FaArrowDown />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollDown} />
-                </Stack>
+                    <Flex w="100%" h="0vh" position="fixed" justifyContent="end">
+                        <IconButton mr={4} icon={isDark ? <FaSun /> : <FaMoon />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={toggleColorMode} />
+                    </Flex>
 
+                    <Flex p={8} direction="row"
+                        alignSelf="center" h="full">
+                        <Box align="flex-start" alignSelf="center">
+                            <Text fontSize={isNotSmallerScreen ? "5xl" : "3xl"} fontWeight="semiBold">
+                                Hi, I'm
+                            </Text>
+                            <Text fontSize={isNotSmallerScreen ? "7xl" : "5xl"} fontWeight="bold" bgGradient="linear(to-r, purple.200, purple.400, purple.600)" bgClip="text">
+                                Felix Wang
+                            </Text>
+                            <Text fontSize={isNotSmallerScreen ? "lg" : "sm"} color={isDark ? "gray.200" : "gray.500"}>
+                                Software Engineer | Computer Science Student @ the Georgia Institute of Technology
+                            </Text>
+                            <Button mt={4} color="purple.400" onClick={() =>
+                                window.open("/Resume.pdf")
+                            }>Resume</Button>
+                            <Button mt={4} ml={4} color="purple.400" onClick={() =>
+                                window.open("mailto:fwang356@gatech.edu")
+                            }>Contact Me!</Button>
+                            <br></br>
+                            <IconButton mt={4} icon={<FaGithub />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("https://www.github.com/fwang356")
+                            } />
+                            <IconButton ml={4} mt={4} icon={<FaLinkedin />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("https://www.linkedin.com/in/fwang356")
+                            } />
+                            <IconButton ml={4} mt={4} icon={<HiOutlineMail />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={() =>
+                                window.open("mailto:fwang356@gatech.edu")
+                            } />
+                        </Box>
+                    </Flex>
+                    <Stack>
+                        <IconButton mb={4} icon={<FaArrowDown />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollDown} />
+                    </Stack>
 
-            </VStack>
+                </VStack>
+            </Div100vh>
             <Stack alignItems="flex-end">
                 <Flex h="0vh" p={4} position="fixed" bottom="5vh">
                     {isVisible && <IconButton icon={<FaArrowUp />} isRound="true" size={isNotSmallerScreen ? "md" : "sm"} onClick={scrollToTop} />}
@@ -96,6 +98,7 @@ export default function Home() {
             <br></br>
             <Experience />
             <Projects />
-        </Div100vh>
+        </div>
+
     )
 }
